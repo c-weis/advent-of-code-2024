c643430ee251c9c6ec153669632723824a993cb2
c-weis <christoph.r.weis@gmail.com>
2024-12-17 23:53:38 +0000

    Day09 part2

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -111,6 +111,78 @@ fn compressed(harddisk: &Vec<DataBlock>) -> Vec<DataBlock> {
     compressed_harddisk
 }
 
+struct MoveInstruction {
+    move_from_idx: usize,
+    move_to_idx: usize,
+    file_id: usize,
+    file_size: usize,
+    remaining_free_size: usize,
+}
+
+fn defrag_compress(harddisk: &mut Vec<DataBlock>) {
+    let mut right_idx = harddisk.len() - 1;
+    while right_idx > 0 {
+        let split_slices = &harddisk.split_at_mut(right_idx);
+        let right_block = &split_slices.1[0];
+        let mut move_instruction: Option<MoveInstruction> = None;
+        match right_block {
+            DataBlock::Free { size: _ } => {
+                right_idx -= 1;
+                continue;
+            },
+            DataBlock::File {
+                id: file_id,
+                size: file_size,
+            } => {
+                for left_idx in 0..right_idx {
+                    let block = &split_slices.0[left_idx];
+                    if let DataBlock::Free { size: free_size } = block {
+                        if *free_size < *file_size {
+                            continue;
+                        }
+
+                        move_instruction = Some(MoveInstruction {
+                            move_from_idx: right_idx,
+                            move_to_idx: left_idx,
+                            file_id: *file_id,
+                            file_size: *file_size,
+                            remaining_free_size: *free_size - *file_size,
+                        });
+
+                        break;
+                    }
+                }
+            }
+        }
+
+        if let Some(MoveInstruction {
+            move_from_idx,
+            move_to_idx,
+            file_id,
+            file_size,
+            remaining_free_size,
+        }) = move_instruction
+        {
+            harddisk[move_from_idx] = DataBlock::Free { size: file_size };
+            harddisk[move_to_idx] = DataBlock::File {
+                id: file_id,
+                size: file_size,
+            };
+
+            if remaining_free_size > 0 {
+                harddisk.insert(
+                    move_to_idx + 1,
+                    DataBlock::Free {
+                        size: remaining_free_size,
+                    },
+                );
+                right_idx += 1;
+            }
+        }
+        right_idx -= 1;
+    }
+}
+
 fn blocks_from_string(string: String) -> Vec<DataBlock> {
     string
         .split("")
@@ -146,8 +218,17 @@ fn part1(path: &str) -> u128 {
     checksum(&compressed_blocks)
 }
 
-fn part2(_path: &str) -> u128 {
-    0
+fn part2(path: &str) -> u128 {
+    let string = lines_from_file(path)
+        .map(|line| line.unwrap())
+        .find_or_first(|_| true)
+        .expect("No input found.");
+
+    let mut blocks = blocks_from_string(string);
+
+    defrag_compress(&mut blocks);
+
+    checksum(&blocks)
 }
 
 #[cfg(test)]
@@ -185,12 +266,40 @@ mod tests {
     }
 
     #[test]
+    fn test_tiny_disks_part2() {
+        // "2": 00 -> 00
+        let mut hdd1 = blocks_from_string(String::from("2"));
+        defrag_compress(&mut hdd1);
+        assert!(checksum(&hdd1) == 0);
+
+        // "232": 00...11 -> 0011...
+        let mut hdd2 = blocks_from_string(String::from("232"));
+        defrag_compress(&mut hdd2);
+        assert!(checksum(&hdd2) == 5);
+
+        // "12345": 0..111....22222 -> 0..111....22222
+        let mut hdd3 = blocks_from_string(String::from("12345"));
+        defrag_compress(&mut hdd3);
+        assert!(
+            checksum(&hdd3)
+                == (partial_checksum(0, 0, 1)
+                    + partial_checksum(1, 3, 3)
+                    + partial_checksum(2, 10, 5)) as u128
+        );
+
+        // "3132": 000.111.. -> 000.111..
+        let mut hdd4 = blocks_from_string(String::from("3132"));
+        defrag_compress(&mut hdd4);
+        assert!(checksum(&hdd4) == 4 + 5 + 6);
+    }
+
+    #[test]
     fn test_part1() {
         assert!(part1("input/input09.txt.test1") == 1928);
     }
 
     #[test]
     fn test_part2() {
-        assert!(part2("input/input09.txt.test1") == 0);
+        assert!(part2("input/input09.txt.test1") == 2858);
     }
 }
